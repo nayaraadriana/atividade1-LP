2eef39f3e0ca95682c71d24a24806d5dc7d56b96
Nayara <dsantos.nay@gmail.com>
2021-03-06 23:57:18 -0300

    cria arquivos do problema1

---
+++ problema1/problema1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main () {
+
+int numeros;
+int tamanhoLista;
+
+printf("Digite o tamanho da lista: \n ");
+scanf ("%i", &tamanhoLista);
+
+return 0;
+
+}
